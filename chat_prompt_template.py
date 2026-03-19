d5baa8ea63db1be85ae12ab5b934d5146adee452
Parleen Bagga <parleenkaurbagga@gmail.com>
2026-03-18 17:03:59 -0700

    Prompt completed

---
+++ chat_prompt_template.py
@@ -0,0 +1,11 @@
+from langchain_core.prompts import ChatPromptTemplate
+
+
+chat_template = ChatPromptTemplate([
+    ('system', "You are a helpful {domain} expert."),
+    ('human', "Explain in simple terms,What is {topic}?")
+
+])
+prompt = chat_template.invoke({'domain':'cricket','topic':'LBW'})
+
+print(prompt)
---
+++ messages.py
@@ -0,0 +1,19 @@
+from langchain_core.messages import SystemMessage, HumanMessage, AIMessage
+from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv
+
+load_dotenv()
+
+model = ChatOpenAI()
+
+messages =[
+    SystemMessage(content="You are a helpful assistant."),
+    HumanMessage(content="What is the capital of India?")
+]
+
+result = model.invoke(messages)
+
+messages.append(AIMessage(content =result.content))
+
+print(messages)
+
